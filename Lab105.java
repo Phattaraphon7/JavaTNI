bb4a3e2189d365172da21f0d7f0a876ad233fd78
Phattaraphon7 <cheerpk62@gmail.com>
2025-12-09 21:28:08 +0700

    LabSheet01

---
+++ Lab105.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,
+                "Summation number =" + (5+2) +
+                "\nSubtraction =" + (2-5) +
+                "\nMultipication =" + (2*5) +
+                "\nDivision number =" + (2/5) +
+                "\nModulus number =" + (2%5),
+                "Arithmetic Operators",
+                JOptionPane.WARNING_MESSAGE);
+    }
+}
