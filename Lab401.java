2f429a5da635d7e5868624d4be5c73ea956f1898
Phattaraphon7 <cheerpk62@gmail.com>
2025-12-09 21:19:43 +0700

    Phattaraphon LabSheet4

--- Lab401.java
+++ Lab401.java
@@ -2,19 +2,13 @@ import javax.swing.*;
 
 public class Lab401 {
     static void main() {
+        String fullName = JOptionPane.showInputDialog("Enter your full name:");
 
+        if(fullName == null || fullName.trim().equals("")) {
+            JOptionPane.showMessageDialog(null, "Please enter your full name!");
+            return;
+        }
 
-                String fullName = JOptionPane.showInputDialog("Enter your full name: ");
-
-                if(fullName == null || fullName.trim().equals("")) {
-                    JOptionPane.showMessageDialog(null, "Please enter your full name!: ");
-                    return;
-                }
-
-                JOptionPane.showMessageDialog(null, "Welcome, " + fullName);
-            }
-
-
+        JOptionPane.showMessageDialog(null, "Welcome, " + fullName);
     }
-
-
+}
